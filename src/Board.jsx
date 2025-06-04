3649c18ab16f903017b23fdd8ede32cba46c989c
Khoi <khoidang01234@gmail.com>
2025-06-04 15:09:10 +0700

    Add react file

---
+++ src/Board.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+
+export default function Board({ getScore, getBestScore }) {
+  const [cards, setCards] = useState([]);
+  const [history, setHistory] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemon() {
+      try {
+        const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=12");
+        const data = await res.json();
+
+        const detailsList = await Promise.all(
+          data.results.map(async (pokemon) => {
+            const res = await fetch(pokemon.url);
+            const detail = await res.json();
+            return {
+              name: detail.name,
+              image: detail.sprites.front_default,
+            };
+          })
+        );
+
+        setCards(detailsList);
+      } catch (error) {
+        console.error("Error fetching Pokémon:", error);
+      }
+    }
+
+    fetchPokemon();
+  }, []);
+
+  const shuffledCards = [...cards].sort(() => 0.5 - Math.random());
+
+  const calculateScore = (card) => {
+    const hasClicked = history.some((item) => item.name === card.name);
+
+    if (hasClicked) {
+      getBestScore();
+      setHistory([]);
+    } else {
+      getScore();
+      setHistory([...history, card]);
+    }
+  };
+
+  return (
+    <div className="board">
+      {shuffledCards.map((card) => (
+        <div onClick={() => calculateScore(card)} className="card" key={card.name}>
+          <img src={card.image} alt={card.name} />
+          <p>{card.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
---
+++ src/Game.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Board from "./Board";
+
+export default function Game() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  function getBestScore() {
+    if (score > bestScore) {
+      setBestScore((prev) => Math.max(prev, score));
+      setScore(0);
+    }
+  }
+
+  return (
+    <div className="game">
+      <div className="header">
+        <div>
+          <h1>Pokemon Memory Game</h1>
+          <p>
+            Get points by clicking on an image but don't click on any more than
+            once!
+          </p>
+        </div>
+        <div>
+          <p>Score: {score}</p>
+          <p>Best score: {bestScore}</p>
+        </div>
+      </div>
+      <Board getScore={() => setScore(score + 1)} getBestScore={getBestScore} />
+    </div>
+  );
+}
